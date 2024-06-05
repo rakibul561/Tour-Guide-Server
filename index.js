f0dd45c8389bda8e30ac71dd0a7426415ab3314d
Rakib <rakibulhasan3929@gmail.com>
2024-06-05 22:53:58 +0600

    added deleits operation

--- index.js
+++ index.js
@@ -31,24 +31,66 @@ async function run() {
         await client.connect();
 
         const menuCollection = client.db("tourUser").collection("menu");
+        const userCollection = client.db("tourUser").collection("users");
+
+        //    users relative api 
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
 
 
-        app.get('/menu', async(req,res)=>{
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null });
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin',
+                    role: 'Tour Guide'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result)
+        })
+
+
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        // menu relative api 
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
-        
+
         // cart details pagse
-        app.get('/menu/:id', async (req, res)=>{
+        app.get('/menu/:id', async (req, res) => {
             const id = req.params.id;
-           const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
 
-           const options = {
-            projection: { description: 1, trip_title:1, relative_information:1,  price: 1, images: 3, },
-          }
+            const options = {
+                projection: { description: 1, trip_title: 1, relative_information: 1, price: 1, images: 3, },
+            }
 
-           const result = await menuCollection.findOne(query, options);
-           res.send(result)
+            const result = await menuCollection.findOne(query, options);
+            res.send(result)
         })
 
 
