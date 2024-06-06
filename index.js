cbba138663ce0a30fd7b5dfb5a45f6fbe7dc7afc
Rakib <rakibulhasan3929@gmail.com>
2024-06-06 19:03:11 +0600

    added single user

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmdvppd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,40 +42,62 @@ async function run() {
         // verified token 
         const verifyToken = (req, res, next) => {
             console.log('inside verify token', req.headers.authorization);
-            if(!req.headers.authorization){
-                return res.status(401).send({message:'forbiden-access'})
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbiden-access' })
             }
             const token = req.headers.authorization.split(' ')[1]
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-                if(err){
-                    return res.status(401).send({message:'forbiden access'})
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbiden access' })
                 }
                 req.decoded = decoded;
-                 next();
+                next();
             })
-           
+
+        }
+
+        //  use verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbiden access' });
+            }
+            next();
         }
 
         // users relative api 
-        app.get('/users', verifyToken, async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             console.log(req.headers);
             const result = await userCollection.find().toArray();
             res.send(result);
         });
+        
 
-        app.get('/users/admin/:email', verifyToken, async (req, res) =>{
-         const email = req.params.email;
-         if(email !== req.decoded.email){
-            return res.status(403).send({message:'unauthorized access'})
-         }
+        app.get('/users/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorized access' })
+            }
 
-         const query = {email: email};
-         const user = await userCollection.findOne(query);
-         let admin = false;
-         if(user){
-            admin = user?.role === 'admin';
-         }
-         res.send({admin});
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+
+        app.get("/single-user/:email", async (req, res) => {
+            const { email } = req.params
+            const user = await userCollection.findOne({ email })
+            if (!user) {
+                return res.status(404).send({ error: "User not found with this email" });
+            }
+            res.status(200).send(user)
         })
 
 
@@ -112,7 +131,7 @@ async function run() {
 
 
 
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
